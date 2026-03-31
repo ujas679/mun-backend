c3fe2f5029d9d74e78276eb59d06537fcc188ac3
ujas gondaliya <83489368+ujas679@users.noreply.github.com>
2026-03-31 14:44:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ app.get("/registrations", async (req, res) => {
     }
 });
 
-// Server Start
-app.listen(3000, () => {
-    console.log("Server running on port 3000 🚀");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
